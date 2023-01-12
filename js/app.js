a51caf954da3c346ee45206829d10aa3c7143703
ahmad <tohirovahmad07@gmail.com>
2023-01-12 14:33:52 +0500

    first coommit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// const pru = confirm("hov are you?")
+// console.log(pru);
+
+// const place = prompt("sening isming kim", "samar")
+// console.log(place)
+
+
+//  const favouriteColor = [];
+
+//  favouriteColor[0] = prompt("wat is your name #1", "");
+//  favouriteColor[1] = prompt("wat is your name #2", "");
+//  favouriteColor[2] = prompt("wat is your name #3", "");
+
+// console.log(favouriteColor);
+
+// const channel = prompt("wat is your youtuber");
+// console.log("https://youtube.com/" + channel);
+
+//interpalat usuli
+// const channel = prompt("wat is your youtuber");
+// console.log(`https://youtube.com/${channel}`);
+
+
